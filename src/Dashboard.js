7bdfefc81f808abba4cb17ff28a232c2f190852f
erk-riv <erk.rivs@gmail.com>
2023-02-02 17:50:13 -0500

    project start

---
+++ src/Dashboard.js
@@ -0,0 +1,31 @@
+import './Dashboard.css';
+import Topbar from './global/Topbar.js';
+import { Box, Typography } from "@mui/material";
+
+
+function App() {
+  return (
+    <div className="Dashboard">
+      <Topbar />
+      <h1>DASDASD</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+// import React from 'react';
+// //import './index.css';
+// import { Box, Typography } from "@mui/material";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>"DSAD"</h1>
+//       <Typography variant="h1">Hello World</Typography>
+//     </div>
+//   );
+// }
+
+// export default App;
